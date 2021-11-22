6ee177382976fe3ee7971f73c522b242154b8005
rodrigosilvab197 <rodrigosilvab197@gmail.com>
2021-11-22 20:34:05 +0000

    Solve Clockwall

--- labs/clockwall/clockServer.go
+++ labs/clockwall/clockServer.go
@@ -1,35 +1,47 @@
 // Clock Server is a concurrent TCP server that periodically writes the time.
 package main
-
 import (
 	"io"
 	"log"
 	"net"
 	"time"
+	"os"
+	"fmt"
 )
-
-func handleConn(c net.Conn) {
+func handleConn(c net.Conn, tZone string) {
 	defer c.Close()
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, timeErr := time.LoadLocation(tZone)
+		if timeErr != nil{
+			log.Print(timeErr)
+			break
+		}
+		_, err := io.WriteString(c, tZone+" "+time.Now().Format("15:04:05\n"))
 		if err != nil {
+			log.Print(err)
 			return // e.g., client disconnected
 		}
 		time.Sleep(1 * time.Second)
 	}
 }
-
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+	args := os.Args[1:]
+	if len(args) != 2 {
+		fmt.Println("Insuficient Parameters !")
+		os.Exit(1)
+	}
+	lHost := "localhost:" + args[1]
+	listener, err := net.Listen("tcp", lHost)
 	if err != nil {
 		log.Fatal(err)
 	}
+	tZone := os.Getenv("TZ")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		go handleConn(conn) // handle connections concurrently
+		go handleConn(conn, tZone) // handle connections concurrently
 	}
 }
